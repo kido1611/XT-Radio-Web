634d8adbe10740306c21ed65e1b0f7a158ad27ae
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2019-07-18 07:15:38 +0700

    Add base view layout

---
+++ resources/views/base/base.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="id">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+        @section('assetscss')
+            
+        @show
+
+        <script src="/assets/js/jquery-3.4.1.min.js"></script>
+
+        <title>
+            @section('titile')
+                XT Radio
+            @show
+        </title>
+    </head>
+    <body>
+        @section('container')
+            
+        @show
+        <script src="/assets/js/popper.min.js"></script>
+        <script src="/assets/js/bootstrap.min.js"></script>
+
+        @section('assetsjs')
+            
+        @show
+    </body>
+</html>
