e8a156c9f16ca6d2ce8498ab693f02a9adb962fc
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2019-10-28 07:39:25 +0700

    Update favicon load image

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     
-    <link rel="shortcut icon" href="assets/images/favicon.ico" />
+    <link rel="shortcut icon" href="/favicon.ico" />
     
     <!-- HTML Meta Tags -->
     <title>XT Radio</title>
