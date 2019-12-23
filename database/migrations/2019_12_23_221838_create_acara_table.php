360bed1d53e532fc44a3982a07e14a87a45f7270
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2019-12-24 02:17:53 +0700

    Add new migrate for acara table

---
+++ database/migrations/2019_12_23_221838_create_acara_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAcaraTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jadwal', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('hari');
+            $table->time('jam_awal');
+            $table->time('jam_akhir');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('jadwal');
+    }
+}
