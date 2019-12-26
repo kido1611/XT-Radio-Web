b585310ff7cf0e3c4f610f996c29e1f743360256
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2019-12-26 15:13:17 +0700

    Fix typo

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,7 @@
 @section('container')
     <div class="row">
         <div class="col-12 mb-5 text-light">
-            <h1>Sedang dibangun</h1>
+            <h1>Selamat Datang</h1>
         </div>
     </div>
 @endsection
