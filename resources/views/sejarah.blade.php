b80fc1f5daee47177ba5d6521e162f6556edda00
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2020-02-22 10:14:16 +0700

    Add warning message on history page

--- resources/views/sejarah.blade.php
+++ resources/views/sejarah.blade.php
@@ -1,7 +1,9 @@
 @extends('base.beta')
 @section('container')
 <div class="container text-center w-100">
-Sejarah XT
+    <h3 class="text-center">SEJARAH XT</h3>
+
+    <h5 class="text-warning">Sedang dibangun</h5>
 </div>
 @endsection
 
