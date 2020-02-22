19d90194f901200d8ce33cb86fd818472542b69e
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2020-02-22 10:38:44 +0700

    Add font on beta base

--- resources/views/base/beta.blade.php
+++ resources/views/base/beta.blade.php
@@ -33,6 +33,13 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
+        <style>
+            *{
+                font-family: 'Open Sans',Helvetica,Arial,sans-serif;
+            }
+        </style>
+
         <!-- Custom CSS -->
         <style type="text/css">
             body{
